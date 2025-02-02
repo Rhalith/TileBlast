6c890f5982dcde886f2e129c93ab61cafb4285e6
Rhalith <81980850+Rhalith@users.noreply.github.com>
2025-02-02 18:51:55 +0300

    Add: level selection and level data basis.

---
+++ Assets/_GAME/Scripts/Level/LevelChanger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Level
+{
+    public class LevelChanger : MonoBehaviour
+    {
+        [SerializeField] private int startLevel;
+        [SerializeField] private List<Button> levelButtons;
+        private void Start()
+        {
+            PlayerPrefs.SetInt("Level", startLevel);
+            
+            for (int i = 0; i < levelButtons.Count; i++)
+            {
+                if (i <= PlayerPrefs.GetInt("Level")-1)
+                {
+                    levelButtons[i].interactable = true;
+                }
+                else
+                {
+                    levelButtons[i].interactable = false;
+                }
+            }
+        }
+
+        public void ChangeLevelData(int i)
+        {
+            PlayerPrefs.SetInt("Level", i);
+        }
+    }
+
+}
---
+++ Assets/_GAME/Scripts/Level/LevelData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Scripts.Level
+{
+
+    [CreateAssetMenu(fileName = "NewLevelData", menuName = "Game/Level Data")]
+    public class LevelData : ScriptableObject
+    {
+        [SerializeField] private int rows; // Grid rows (M)
+        [SerializeField] private int columns; // Grid columns (N)
+        [SerializeField] private int numColors; // Number of colors (K)
+        [SerializeField] private int groupThresholdA; // Threshold for first icon (A)
+        [SerializeField] private int groupThresholdB; // Threshold for second icon (B)
+        [SerializeField] private int groupThresholdC; // Threshold for third icon (C)
+
+        public int Rows => rows;
+        public int Columns => columns;
+        public int NumColors => numColors;
+        public int GroupThresholdA => groupThresholdA;
+        public int GroupThresholdB => groupThresholdB;
+        public int GroupThresholdC => groupThresholdC;
+    }
+
+}
