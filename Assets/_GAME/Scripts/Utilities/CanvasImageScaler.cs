4d788561656081a217690df15fe8861b18e555f7
Rhalith <81980850+Rhalith@users.noreply.github.com>
2025-02-05 12:08:53 +0300

    Add: backgrounds for both scene and scaler

---
+++ Assets/_GAME/Scripts/Utilities/CanvasImageScaler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Utilities
+{
+
+    public class CanvasImageScaler : MonoBehaviour
+    {
+        [SerializeField] private Image targetImage;
+
+        private void Start()
+        {
+            FitImageToScreen();
+        }
+
+        private void FitImageToScreen()
+        {
+            if (targetImage == null) return;
+
+            RectTransform imageRectTransform = targetImage.rectTransform;
+            float screenRatio = (float)Screen.width / Screen.height;
+            float imageRatio = targetImage.sprite.bounds.size.x / targetImage.sprite.bounds.size.y;
+
+            if (screenRatio > imageRatio)
+            {
+                imageRectTransform.sizeDelta = new Vector2(Screen.width, Screen.width / imageRatio);
+            }
+            else
+            {
+                imageRectTransform.sizeDelta = new Vector2(Screen.height * imageRatio, Screen.height);
+            }
+        }
+    }
+
+}
